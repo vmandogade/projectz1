2fed9c9a455cc4bc9a9e8c8434d22941e4129539
“Vaibhav <vmandogade@gmail.com>
2023-07-03 13:48:36 +0530

    app.py file, templates folder, final project done

---
+++ Desktop/ProjectZ1/gemstone/src/pipeline/prediction_pipeline.py
@@ -0,0 +1,71 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+import pandas as pd
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')  # preprocessor pkl file path
+            model_path=os.path.join('artifacts','model.pkl')  # model pkl file path
+
+            preprocessor=load_object(preprocessor_path) # obtaining preprocessor pkl file
+            model=load_object(model_path)  # obtaining model pkl file
+
+            data_scaled=preprocessor.transform(features)  #applying preprocessing on new data entered by the user
+
+            pred=model.predict(data_scaled)  #applying model object on a new data entered by the user
+            return pred
+            
+
+        except Exception as e:
+            logging.info("Exception occured in prediction")
+            raise CustomException(e,sys)
+        
+class CustomData:
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+
+    def get_data_as_dataframe(self):  #we have build model on the basis of dataframe, so need to capture 
+        try:                          #new data in the form of dataframe
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
